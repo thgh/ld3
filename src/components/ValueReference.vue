fa2f5db531257365575a94c0f9c8937c1c123898
Thomas <hello@thomasg.be>
2016-03-28 11:25:25 +0200

    Fix reference value

--- src/components/ValueReference.vue
+++ src/components/ValueReference.vue
@@ -6,6 +6,11 @@
 export default {
   name: 'value-reference',
   props: ['fragment', 'prop', 'index'],
+  computed: {
+    value () {
+      return typeof this.index === 'number' ? this.fragment[this.prop][this.index] : this.fragment[this.prop]
+    }
+  },
   ready () {
     // console.log('PE', this.fragment)
     console.log(this)
