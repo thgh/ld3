792e6a645c81c1285e02484deff57d811d0024b3
Thomas <hello@thomasg.be>
2016-04-17 14:46:00 +0200

    Search filter for recent fragments

--- src/components/RecentFragments.vue
+++ src/components/RecentFragments.vue
@@ -1,20 +1,39 @@
 <template>
-	<section>
-	  <h2>recent fragments</h2>
-	  <div v-for="(uri, fragment) in list">
-	    <a href="#!{{uri}}" class="a-fragment">
-	    	{{ fragment['schema:name'] || fragment.name || 'Unnamed' }}
-	    	<small>{{ uri }}</small>
-	    </a>
-	  </div>
-	</section>
+  <section>
+    <h2>Fragments</h2>
+    <div class="inp-text inp-search">
+      <input type="text" v-model="search" placeholder="Search...">
+    </div>
+    <div v-for="(uri, fragment) in list | filterBy search" transition="staggered">
+      <a href="#!{{uri}}" class="a-fragment">
+        {{ fragment['schema:name'] || 'Unnamed' }}
+        <small>{{ uri }}</small>
+      </a>
+    </div>
+  </section>
 </template>
 
 <script>
 export default {
   props: ['list', 'route'],
+  data: {
+    search: ''
+  },
   ready () {
     // console.log('recent', this.$route.path)
   }
 }
 </script>
+
+<style>
+.staggered-transition {
+  transition: all .5s ease;
+  overflow: hidden;
+  margin: 0;
+  height: 3em;
+}
+.staggered-enter, .staggered-leave {
+  opacity: 0;
+  height: 0;
+}
+</style>
