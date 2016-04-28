106808a5203b0e1d1907a9da196f2fd309f12ddd
Thomas <hello@thomasg.be>
2016-04-29 00:46:25 +0200

    ValueText on a newline

--- src/components/ValueText.vue
+++ src/components/ValueText.vue
@@ -11,9 +11,33 @@ export default {
     textarea.oninput = function () {
       // 25px happens to work best
       textarea.style.height = '25px'
-      textarea.style.height = 1 + Math.min(textarea.scrollHeight, 500) + 'px'
+      var scroll = textarea.scrollHeight
+      if (textarea.parentNode.classList.contains('inp-block')) {
+        if (scroll < 30) {
+          textarea.parentNode.classList.remove('inp-block')
+          scroll = textarea.scrollHeight
+        }
+      } else if (scroll > 50) {
+        textarea.parentNode.classList.add('inp-block')
+        scroll = textarea.scrollHeight
+      }
+      textarea.style.height = 1 + Math.min(scroll, 500) + 'px'
     }
     textarea.oninput()
   }
 }
 </script>
+
+<style>
+.inp-block {
+  display: block;
+}
+.inp-block>.inp-label {
+  float: none;
+}
+.inp-block>textarea {
+  margin-top: 8px;
+  margin-left: 12px;
+  width: calc(100% - 12px);
+}
+</style>
