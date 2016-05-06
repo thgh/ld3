f9aa33e0274a5e5c4b6ecabb17ab573dcd5733e3
Thomas <hello@thomasg.be>
2016-05-07 00:25:58 +0200

    Separate router from App.vue

--- src/App.vue
+++ src/App.vue
@@ -8,65 +8,15 @@ import Home from './components/Home'
 import Conf from './components/Conf'
 import Edit from './components/Edit'
 
+import Router from './mixins/Router.js'
+
 export default {
   data () {
-    if (window.location.pathname !== '/') {
-      console.log('replaceState', window.location.pathname)
-      if (window.location.hash.length) {
-        window.history.replaceState('wut', 'ld3', '/' + window.location.hash)
-      } else {
-        window.history.replaceState('wut', 'ld3', '/#!' + window.location.pathname.substr(1))
-      }
-    }
     return {
-      route: {
-        view: 'home',
-        uri: ''
-      },
       namespaces: {}
     }
   },
-  methods: {
-    hashchange () {
-      let hash = window.location.hash
-      if (hash.length === 0) {
-        console.log('no hash')
-      } else if (hash.length === 1 && hash !== '#') {
-        console.warn('not much hash but umm', hash)
-      } else if (hash.length === 2) {
-        console.log('not much hash')
-      } else if (hash.substr(0, 2) === '#!') {
-        hash = hash.substr(2)
-        if (hash.indexOf(':') === -1) {
-          console.log('page', hash)
-          if (hash !== 'conf' && hash !== 'home' && hash !== 'edit') {
-            console.log('  or', hash)
-            hash = 'home'
-          }
-          this.route.view = hash
-          window.document.title = 'ld3 ' + hash
-        } else {
-          console.log('goto ', hash)
-          this.route.view = 'edit'
-          this.route.uri = this.$root.ns.min(hash)
-          window.document.title = 'ld3:edit ' + this.route.uri
-        }
-      } else {
-        console.log('changed hash to ', hash)
-      }
-    }
-  },
-  ready () {
-    this.hashchange()
-  },
-  attached () {
-    // Start routing
-    window.addEventListener('hashchange', this.hashchange, false)
-  },
-  detached () {
-    // Stop routing
-    window.removeEventListener('hashchange', this.hashchange, false)
-  },
+  mixins: [Router],
   components: {
     Home,
     Conf,
---
+++ src/mixins/Router.js
@@ -0,0 +1,77 @@
+var hostPaths = {
+  '//thomasg.be': '/ld3/',
+  '//localhost/open/ld3/dist': '/open/ld3/dist/'
+}
+
+export default {
+  data () {
+    // Use homepage as default because it is lightweight
+    var route = {
+      base: '/',
+      uri: '',
+      view: 'home'
+    }
+    // Detect installation directory
+    if (window.location.pathname !== '/') {
+      for (var host in hostPaths) {
+        console.log(window.location.href, host)
+        if (window.location.href.indexOf(host) !== -1) {
+          route.base = hostPaths[host]
+          continue
+        }
+      }
+      // Replace url to get the router to start properly
+      if (window.location.hash.length) {
+        console.log('replace base url', route.base)
+        window.history.replaceState('wut', 'ld3', route.base + window.location.hash)
+      } else if (window.location.pathname.length > route.base.length) {
+        console.log('replace complete url', route.base)
+        window.history.replaceState('wut', 'ld3', route.base + '#!' + window.location.href)
+      }
+    }
+    return {
+      route: route
+    }
+  },
+  methods: {
+    hashchange () {
+      let hash = window.location.hash
+      if (hash.length === 0) {
+        console.log('no hash')
+      } else if (hash.length === 1 && hash !== '#') {
+        console.warn('not much hash but umm', hash)
+      } else if (hash.length === 2) {
+        console.log('not much hash')
+      } else if (hash.substr(0, 2) === '#!') {
+        hash = hash.substr(2)
+        if (hash.indexOf(':') === -1) {
+          console.log('page', hash)
+          if (hash !== 'conf' && hash !== 'home' && hash !== 'edit') {
+            console.log('  or', hash)
+            hash = 'home'
+          }
+          this.route.view = hash
+          window.document.title = 'ld3 ' + hash
+        } else {
+          console.log('goto ', hash)
+          this.route.view = 'edit'
+          this.route.uri = this.$root.ns.min(hash)
+          window.document.title = 'ld3:edit ' + this.route.uri
+        }
+      } else {
+        console.log('changed hash to ', hash)
+      }
+    }
+  },
+  ready () {
+    this.hashchange()
+  },
+  attached () {
+    // Start routing
+    window.addEventListener('hashchange', this.hashchange, false)
+  },
+  detached () {
+    // Stop routing
+    window.removeEventListener('hashchange', this.hashchange, false)
+  }
+}
