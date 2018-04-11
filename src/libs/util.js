9e7c186a40463c22a5262de4f31e2579f786bd37
Thomas Ghysels <info@thomasg.be>
2018-04-11 23:38:20 +0200

    Use ValueText for numbers

--- src/libs/util.js
+++ src/libs/util.js
@@ -4,7 +4,7 @@ export function inert (x) {
 
 export function toType (o) {
   return !o || typeof o !== 'object'
-    ? (typeof o === 'boolean' ? 'ValueString' : typeof o === 'number' ? 'ValueString' : 'ValueText')
+    ? (typeof o === 'boolean' ? 'ValueString' : typeof o === 'number' ? 'ValueText' : 'ValueText')
     : Array.isArray(o) ? 'ValueArray' : o['@id'] && o['@id'].charAt(0) !== '_' ? 'ValueReference'
       : typeof o['@value'] !== 'undefined' ? 'ValueLiteral' : 'ValueObject'
 }
